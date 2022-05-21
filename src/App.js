d7b8136c4d23e428451230a8a1e7e001cf87d3fd
Jose <jcoronadoar@gmail.com>
2022-05-21 19:24:39 +0200

    4.1. Cargamos el data en App.js para recorrer con un map todo el array de datos y pasarlo a TrelloList para generar cada lista con sus datos

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import TrelloList from "./components/TrelloList";
 import AddCardList from "./components/AddCardList";
 import Logo from "./assets/img/logo-app.png";
 import {fade, makeStyles} from "@material-ui/core";
+import dataApp from "./dataApp";
+import {useState} from "react";
 
 function App() {
   const classes = uneStyle();
+  const [data, setData] = useState(dataApp);
+    console.log(data)
   return (
     <div className="App">
       <header>
           <img src={Logo}/>
       </header>
         <div className={classes.container}>
-            <TrelloList></TrelloList>
-            <TrelloList></TrelloList>
-            <TrelloList></TrelloList>
+
+            {
+                data.listIds.map(listID => {
+                    const list = data.lists[listID]
+                    return <TrelloList list={list} key={listID}/>
+                })
+            }
+
             <AddCardList type="list"/>
         </div>
     </div>
