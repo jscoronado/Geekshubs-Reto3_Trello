e62db614adb60f33005c8f90255be615a3c507c6
Jose <jcoronadoar@gmail.com>
2022-05-21 19:23:10 +0200

    4. Añadimos dataApp.js donde incluiremos un array de datos con las listas inciciales y sus tareas que se cargaran por defecto a modo base de datos.

---
+++ src/dataApp.js
@@ -0,0 +1,35 @@
+const dataApp = {
+    lists: {
+        "list1": {
+            id: "list1",
+            title: "Lista de tareas",
+            cards: [
+                {
+                    id: "list1_card1",
+                    title: "Desarrollar Back"
+                },
+                {
+                    id: "list1_card2",
+                    title: "Desarrollar Front"
+                },
+                {
+                    id: "list1_card3",
+                    title: "Enviar Proyecto"
+                },
+            ]
+        },
+        "list2": {
+            id: "list2",
+            title: "En proceso",
+            cards: []
+        },
+        "list3": {
+            id: "list3",
+            title: "Hecho",
+            cards: []
+        },
+    },
+    listIds: ["list1", "list2", "list3"]
+}
+
+export default dataApp
